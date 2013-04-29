7cf30b7de9d12258f20b1a51cd9f6a21f687bcf4
Matt Parker <mattmparker@gmail.com>
2013-04-29 11:13:36 -0600

    Auto-archive data and file reports in a subdir
    Trying to make this as user-friendly as possible; you should know be able to
    just drag the SurveyMonkey archive to this folder and double-click the batch
    script.  Not exactly for beginners, but it's a step in the right direction...

--- generate_report.r
+++ generate_report.r
@@ -5,11 +5,37 @@
 options(stringsAsFactors = FALSE)
 
 
-# Load knitr to generate the report
+# Load knitr and markdown to generate the report
 library(knitr)
+library(markdown)
+
+# Identify the zip file containing the results
+datafile <- list.files(pattern = "*.zip")
+
+# Extract the collector list
+collectors <- read.csv(unz(datafile, file.path("CSV", "CollectorList.csv")))
 
 
-# Knit it - even though the .rmd file is located elsewhere, it will
-# run with the data in this folder
-knit2html(input = "qi_survey_report.rmd")
+# Get the name of the first collector - if probably formatted, you should
+# be able to strip the strategic area abbreviation from it
+# Pattern: any non-space characters from the beginning until the first space
+strat_area <- gsub(x = collectors$Title,
+                   pattern = "(^\\S*)\\s.*",
+                   replacement = "\\1")[1]
+
+
+# Generate a name for the report output file
+report_name <- paste(strat_area, "_", Sys.Date(), ".html", sep = "")
+
+
+
+# Knit it
+knit("qi_survey_report.rmd")
+
+markdownToHTML(file = "qi_survey_report.md",
+               output = file.path("reports", report_name))
+
+
+# Remove the residual .md file
+file.remove(list.files(pattern = "qi_survey_report.md"))
      
